d8dfba4725b1d90e0dc1327067ff3f300c109a8e
caaarrrrlll <carlosmoreta140@gmail.com>
2025-07-06 14:13:05 -0500

    Add project files.

---
+++ People/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace People
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
